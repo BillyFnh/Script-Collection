d1343e6b41ff0116777f0882162c9a52191c3f99
root <billyfong2007@hotmail.com>
2020-08-27 11:05:43 +0800

    addeed script for cleaning up k8s stale replica sets

---
+++ macroview/k8s-cleanup-stale-replica-set.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# clean up stale Replica Sets on K8s cluster
+# Cron Schedule: 15 4 * * *
+
+# cleanup namespace: splunk-database & production-splunk-database
+IFS=" " read -r -a rsNameSpaceSplunk <<< `echo $(kubectl get replicaset.apps --all-namespaces | grep -E -o ".*splunk-database.*0\s+0" | grep -E -o "^\S+")`
+IFS=" " read -r -a rsNameSplunk <<< `echo $(kubectl get replicaset.apps --all-namespaces | grep -E -o "splunk-database.*0\s+0" | grep -E -o "\s+\S{3,99}\s+" | grep -E -o "\S*")`
+
+for (( i=0 ; i<${#rsNameSplunk[@]} ; i++ ));do
+    echo $(kubectl delete replicaset.apps/${rsNameSplunk[i]} -n ${rsNameSpaceSplunk[i]})
+done
+
+# cleanup namespace: defacement-detection & production-defacement-detection
+IFS=" " read -r -a rsNameSpaceDefacement <<< `echo $(kubectl get replicaset.apps --all-namespaces | grep -E -o ".*defacement-detection.*0\s+0" | grep -E -o "^\S+")`
+IFS=" " read -r -a rsNameDefacement <<< `echo $(kubectl get replicaset.apps --all-namespaces | grep -E -o "defacement-detection.*0\s+0" | grep -E -o "\s+\S{3,99}\s+" | grep -E -o "\S*")`
+
+for (( i=0 ; i<${#rsNameDefacement[@]} ; i++ ));do
+    echo $(kubectl delete replicaset.apps/${rsNameDefacement[i]} -n ${rsNameSpaceDefacement[i]})
+done
+
+curl -X POST -H 'Content-type: application/json' --data '{"text": "*Cleanup Stale Replica Sets*\n*Target Namespaces*:\t\t splunk-database\n\t\t\t\t\t\t\t\t\t\t\tproduction-splunk-database\n\t\t\t\t\t\t\t\t\t\t\tdefacement-detection\n\t\t\t\t\t\t\t\t\t\t\tproduction-defacement-detection\n"}' https://hooks.slack.com/services/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
